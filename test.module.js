b7427cae8b6538c25c35b4255e72ebe4aaa2040f
johnlenon <johnlenonmaghanoy@gmail.com>
2017-09-20 13:14:56 +0800

    Added tests.

--- test.module.js
+++ test.module.js
@@ -455,6 +455,286 @@ describe( "objo", ( ) => {
 		} );
 	} );
 
+	describe( "`objo( [ 'hello' ] ).toString( )`", ( ) => {
+		it( "should return string type", ( ) => {
+			assert.equal( typeof objo( [ "hello" ] ).toString( ), "string" );
+		} );
+	} );
+
+	describe( "`objo( [ 1 ] ).toString( )`", ( ) => {
+		it( "should return string type", ( ) => {
+			assert.equal( typeof objo( [ 1 ] ).toString( ), "string" );
+		} );
+	} );
+
+	describe( "`objo( [ Infinity ] ).toString( )`", ( ) => {
+		it( "should return string type", ( ) => {
+			assert.equal( typeof objo( [ Infinity ] ).toString( ), "string" );
+		} );
+	} );
+
+	describe( "`objo( [ NaN ] ).toString( )`", ( ) => {
+		it( "should return string type", ( ) => {
+			assert.equal( typeof objo( [ NaN ] ).toString( ), "string" );
+		} );
+	} );
+
+	describe( "`objo( [ true ] ).toString( )`", ( ) => {
+		it( "should return string type", ( ) => {
+			assert.equal( typeof objo( [ true ] ).toString( ), "string" );
+		} );
+	} );
+
+	describe( "`objo( [ { 'hello': 'world' } ] ).toString( )`", ( ) => {
+		it( "should return string type", ( ) => {
+			assert.equal( typeof objo( [ { "hello": "world" } ] ).toString( ), "string" );
+		} );
+	} );
+
+	describe( "`objo( [ null ] ).toString( )`", ( ) => {
+		it( "should return string type", ( ) => {
+			assert.equal( typeof objo( [ null ] ).toString( ), "string" );
+		} );
+	} );
+
+	describe( "`objo( [ Symbol.for( 'hello' ) ] ).toString( )`", ( ) => {
+		it( "should return string type", ( ) => {
+			assert.equal( typeof objo( [ Symbol.for( "hello" ) ] ).toString( ), "string" );
+		} );
+	} );
+
+	describe( "`objo( [ 'hello' ] ).toNumber( )`", ( ) => {
+		it( "should return number type", ( ) => {
+			assert.equal( typeof objo( [ "hello" ] ).toNumber( ), "number" );
+		} );
+	} );
+
+	describe( "`objo( [ 1 ] ).toNumber( )`", ( ) => {
+		it( "should return number type", ( ) => {
+			assert.equal( typeof objo( [ 1 ] ).toNumber( ), "number" );
+		} );
+	} );
+
+	describe( "`objo( [ Infinity ] ).toNumber( )`", ( ) => {
+		it( "should return number type", ( ) => {
+			assert.equal( typeof objo( [ Infinity ] ).toNumber( ), "number" );
+		} );
+	} );
+
+	describe( "`objo( [ NaN ] ).toNumber( )`", ( ) => {
+		it( "should return number type", ( ) => {
+			assert.equal( typeof objo( [ NaN ] ).toNumber( ), "number" );
+		} );
+	} );
+
+	describe( "`objo( [ true ] ).toNumber( )`", ( ) => {
+		it( "should return number type", ( ) => {
+			assert.equal( typeof objo( [ true ] ).toNumber( ), "number" );
+		} );
+	} );
+
+	describe( "`objo( [ { 'hello': 'world' } ] ).toNumber( )`", ( ) => {
+		it( "should return number type", ( ) => {
+			assert.equal( typeof objo( [ { "hello": "world" } ] ).toNumber( ), "number" );
+		} );
+	} );
+
+	describe( "`objo( [ null ] ).toNumber( )`", ( ) => {
+		it( "should return number type", ( ) => {
+			assert.equal( typeof objo( [ null ] ).toNumber( ), "number" );
+		} );
+	} );
+
+	describe( "`objo( [ Symbol.for( 'hello' ) ] ).toNumber( )`", ( ) => {
+		it( "should return number type", ( ) => {
+			assert.equal( typeof objo( [ Symbol.for( "hello" ) ] ).toNumber( ), "number" );
+		} );
+	} );
+
+	describe( "`objo( [ 'hello' ] ).toBoolean( )`", ( ) => {
+		it( "should return true", ( ) => {
+			assert.equal( objo( [ "hello" ] ).toBoolean( ), true );
+		} );
+	} );
+
+	describe( "`objo( [ 1 ] ).toBoolean( )`", ( ) => {
+		it( "should return true", ( ) => {
+			assert.equal( objo( [ 1 ] ).toBoolean( ), true );
+		} );
+	} );
+
+	describe( "`objo( [ Infinity ] ).toBoolean( )`", ( ) => {
+		it( "should return true", ( ) => {
+			assert.equal( objo( [ Infinity ] ).toBoolean( ), true );
+		} );
+	} );
+
+	describe( "`objo( [ NaN ] ).toBoolean( )`", ( ) => {
+		it( "should return true", ( ) => {
+			assert.equal( objo( [ NaN ] ).toBoolean( ), true );
+		} );
+	} );
+
+	describe( "`objo( [ true ] ).toBoolean( )`", ( ) => {
+		it( "should return true", ( ) => {
+			assert.equal( objo( [ true ] ).toBoolean( ), true );
+		} );
+	} );
+
+	describe( "`objo( [ { 'hello': 'world' } ] ).toBoolean( )`", ( ) => {
+		it( "should return true", ( ) => {
+			assert.equal( objo( [ { "hello": "world" } ] ).toBoolean( ), true );
+		} );
+	} );
+
+	describe( "`objo( [ null ] ).toBoolean( )`", ( ) => {
+		it( "should return true", ( ) => {
+			assert.equal( objo( [ null ] ).toBoolean( ), true );
+		} );
+	} );
+
+	describe( "`objo( [ Symbol.for( 'hello' ) ] ).toBoolean( )`", ( ) => {
+		it( "should return true", ( ) => {
+			assert.equal( objo( [ Symbol.for( "hello" ) ] ).toBoolean( ), true );
+		} );
+	} );
+
+	describe( "`objo( [ 'hello' ] ).toObject( )`", ( ) => {
+		it( "should contain object type", ( ) => {
+			let descriptor = objo( [ "hello" ] ).toObject( )[ 0 ];
+
+			assert.equal( typeof descriptor, "object" );
+
+			assert.equal( "type" in descriptor, true );
+
+			assert.equal( "name" in descriptor, true );
+
+			assert.equal( "value" in descriptor, true );
+
+			assert.equal( "format" in descriptor, true );
+
+		} );
+	} );
+
+	describe( "`objo( [ 1 ] ).toObject( )`", ( ) => {
+		it( "should contain object type", ( ) => {
+			let descriptor = objo( [ 1 ] ).toObject( )[ 0 ];
+
+			assert.equal( typeof descriptor, "object" );
+
+			assert.equal( "type" in descriptor, true );
+
+			assert.equal( "name" in descriptor, true );
+
+			assert.equal( "value" in descriptor, true );
+
+			assert.equal( "format" in descriptor, true );
+
+		} );
+	} );
+
+	describe( "`objo( [ Infinity ] ).toObject( )`", ( ) => {
+		it( "should contain object type", ( ) => {
+			let descriptor = objo( [ Infinity ] ).toObject( )[ 0 ];
+
+			assert.equal( typeof descriptor, "object" );
+
+			assert.equal( "type" in descriptor, true );
+
+			assert.equal( "name" in descriptor, true );
+
+			assert.equal( "value" in descriptor, true );
+
+			assert.equal( "format" in descriptor, true );
+
+		} );
+	} );
+
+	describe( "`objo( [ NaN ] ).toObject( )`", ( ) => {
+		it( "should contain object type", ( ) => {
+			let descriptor = objo( [ NaN ] ).toObject( )[ 0 ];
+
+			assert.equal( typeof descriptor, "object" );
+
+			assert.equal( "type" in descriptor, true );
+
+			assert.equal( "name" in descriptor, true );
+
+			assert.equal( "value" in descriptor, true );
+
+			assert.equal( "format" in descriptor, true );
+
+		} );
+	} );
+
+	describe( "`objo( [ true ] ).toObject( )`", ( ) => {
+		it( "should contain object type", ( ) => {
+			let descriptor = objo( [ true ] ).toObject( )[ 0 ];
+
+			assert.equal( typeof descriptor, "object" );
+
+			assert.equal( "type" in descriptor, true );
+
+			assert.equal( "name" in descriptor, true );
+
+			assert.equal( "value" in descriptor, true );
+
+			assert.equal( "format" in descriptor, true );
+
+		} );
+	} );
+
+	describe( "`objo( [ { 'hello': 'world' } ] ).toObject( )`", ( ) => {
+		it( "should contain object type", ( ) => {
+			let descriptor = objo( [ { "hello": "world" } ] ).toObject( )[ 0 ].hello;
+
+			assert.equal( typeof descriptor, "object" );
+
+			assert.equal( "type" in descriptor, true );
+
+			assert.equal( "name" in descriptor, true );
+
+			assert.equal( "value" in descriptor, true );
+
+			assert.equal( "format" in descriptor, true );
+
+		} );
+	} );
+
+	describe( "`objo( [ null ] ).toObject( )`", ( ) => {
+		it( "should contain object type", ( ) => {
+			let descriptor = objo( [ null ] ).toObject( )[ 0 ];
+
+			assert.equal( typeof descriptor, "object" );
+
+			assert.equal( "type" in descriptor, true );
+
+			assert.equal( "name" in descriptor, true );
+
+			assert.equal( "value" in descriptor, true );
+
+			assert.equal( "format" in descriptor, true );
+
+		} );
+	} );
+
+	describe( "`objo( [ Symbol.for( 'hello' ) ] ).toObject( )`", ( ) => {
+		it( "should contain object type", ( ) => {
+			let descriptor = objo( [ Symbol.for( "hello" ) ] ).toObject( )[ 0 ];
+
+			assert.equal( typeof descriptor, "object" );
+
+			assert.equal( "type" in descriptor, true );
+
+			assert.equal( "name" in descriptor, true );
+
+			assert.equal( "value" in descriptor, true );
+
+			assert.equal( "format" in descriptor, true );
+
+		} );
+	} );
+
 } );
 
 //: @end-client
